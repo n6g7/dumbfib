aa4a2a5bb004dec383290ab2eea3bf50143b3d1b
Nathan Gaberel <nathang@theodo.co.uk>
2017-10-04 20:08:36 +0100

    Add CI.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const crypto = require('crypto');
-const http = require('http');
-const url = require('url');
+const crypto = require('crypto')
+const http = require('http')
+const url = require('url')
 
-function dumbFibonacci(n) {
-  if (n == 0 || n == 1) return 1;
+function dumbFibonacci (n) {
+  if (n === 0 || n === 1) return 1
 
-  return dumbFibonacci(n-1) + dumbFibonacci(n-2);
+  return dumbFibonacci(n - 1) + dumbFibonacci(n - 2)
 }
 
 http.createServer((req, res) => {
-  let { n } = url.parse(req.url, true).query;
+  let { n } = url.parse(req.url, true).query
 
   if (!n) return res.end('Add a query parameter to your request: <a href="?n=2">n=2</a>')
 
-  n = parseInt(n);
-  const reqId = `req-${crypto.randomBytes(2).toString('hex')} (${n})`;
+  n = parseInt(n)
+  const reqId = `req-${crypto.randomBytes(2).toString('hex')} (${n})`
 
-  console.time(reqId);
-  const result = dumbFibonacci(n);
-  console.timeEnd(reqId);
+  console.time(reqId)
+  const result = dumbFibonacci(n)
+  console.timeEnd(reqId)
 
-  res.end(''+result);
-}).listen(8080, () => console.log('Server started.'));
+  res.end('' + result)
+}).listen(8080, () => console.log('Server started.'))
